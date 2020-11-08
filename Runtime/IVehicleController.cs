fffc14d6330e9fdbac5ba7cfa9ecdf7635ac3058
Ivan Notaros <nothke@gmail.com>
2020-11-09 00:30:56 +0100

    Initial

---
+++ Runtime/IVehicleController.cs
@@ -0,0 +1,10 @@
+﻿public interface IVehicleController
+{
+    float accelInput { get; set; }
+    float brakeInput { get; set; }
+    float steerInput { get; set; }
+    float handbrakeInput { get; set; }
+    int turnInput { get; set; }
+
+    float GetForwardVelocity();
+}
