791fe5a26e6580feeea8381593c0e07ad7109478
Ivan Notaros <nothke@gmail.com>
2021-11-01 12:18:41 +0100

    Split IVehicleController to Input and Output

--- Runtime/IVehicleController.cs
+++ Runtime/IVehicleController.cs
@@ -1,4 +1,4 @@
-﻿public interface IVehicleController
+﻿public interface IVehicleInput
 {
     float accelInput { get; set; }
     float brakeInput { get; set; }
@@ -7,6 +7,17 @@
     int turnInput { get; set; }
 
     bool Reverse { get; set; }
+
+    float GetForwardVelocity();
+}
+
+public interface IVehicleOutput
+{
+    float AccelOutput { get; }
+    float BrakeOutput { get; }
+    float SteerOutput { get; }
+    float HandbrakeOutput { get; }
+    int CurrentGear { get; }
 
     float GetForwardVelocity();
 }
